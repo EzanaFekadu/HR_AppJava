32e2f90af9a0c7cdfb95d05b407e1b37e75503e3
EzanaFekadu <151008676+EzanaFekadu@users.noreply.github.com>
2024-02-05 13:25:12 -0600

    EZ 2/5/24 startup

---
+++ src/main/java/com/example/HR_AppJava/HR_App/Startup.java
@@ -0,0 +1,23 @@
+package com.example.HR_AppJava.HR_App;
+
+import java.util.ArrayList;
+
+public class Startup {
+    private ArrayList<String> locationCells;
+    private void setLocationCells(ArrayList<String> locs) {
+		locationCells = locs;
+	}
+	private String checkYourself (String userInput) {
+		String result = "miss";
+		int index = locationCells.indexOf(userInput);
+		if (index >= 0) {
+			locationCells.remove(index);
+			if (locationCells.isEmpty()) {
+				result = "kill";
+				} else {
+				result = "hit";
+			}
+		}
+		return result;
+	}
+}
