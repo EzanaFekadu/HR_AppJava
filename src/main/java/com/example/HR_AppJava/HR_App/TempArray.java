1448c05b92ca72a485fd9d82993eed61c0e55c00
EzanaFekadu <151008676+EzanaFekadu@users.noreply.github.com>
2024-04-20 19:04:58 -0500

    4/20/24 EZ TempArray.java

---
+++ src/main/java/com/example/HR_AppJava/HR_App/TempArray.java
@@ -0,0 +1,22 @@
+package main.java.com.example.HR_AppJava.HR_App;
+
+public class TempArray {
+
+    private object[][] tempArray;
+
+    public TempArray(){
+        this.tempArray = new object[6][];
+
+        this.tempArray[0] = new Integer[]{};
+        this.tempArray[1] = new Integer[]{};
+        this.tempArray[2] = new Integer[]{};
+        this.tempArray[3] = new Integer[]{};
+        this.tempArray[4] = new Integer[]{};
+        this.tempArray[5] = new Integer[]{};
+    }
+
+    public void add(){}
+    public void edit(){}
+    public void delete(){}
+
+}
