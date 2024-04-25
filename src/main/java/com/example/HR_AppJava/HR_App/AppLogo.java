3ccc9c716ccb0cc57f5a5f39426d166a9633d0ef
EzanaFekadu <151008676+EzanaFekadu@users.noreply.github.com>
2024-04-25 18:51:27 -0500

    4/25/24 EZ removed old comment

--- src/main/java/com/example/HR_AppJava/HR_App/AppLogo.java
+++ src/main/java/com/example/HR_AppJava/HR_App/AppLogo.java
@@ -19,49 +19,6 @@ public class AppLogo {
 
                     Graphics2D g2d = (Graphics2D) g;
 
-                    /*
-                     * // Draw sun
-                     * g2d.setColor(Color.YELLOW);
-                     * g2d.fillOval(50, 50, 100, 100);
-                     * 
-                     * // Draw sun rays
-                     * g2d.setColor(Color.BLUE);
-                     * for (int i = 0; i < 360; i += 45) {
-                     * double radian = Math.toRadians(i);
-                     * int x1 = 100 + (int) (Math.cos(radian) * 50);
-                     * int y1 = 100 + (int) (Math.sin(radian) * 50);
-                     * int x2 = 100 + (int) (Math.cos(radian) * 80);
-                     * int y2 = 100 + (int) (Math.sin(radian) * 80);
-                     * g2d.drawLine(x1, y1, x2, y2);
-                     * }
-                     * 
-                     * // Draw smiley face
-                     * g2d.setColor(Color.YELLOW);
-                     * g2d.fillOval(75, 75, 50, 50);
-                     * g2d.setColor(Color.BLUE);
-                     * g2d.drawArc(80, 90, 40, 20, -180, 180);
-                     * 
-                     * // Draw Eyes on the Sun
-                     * g2d.drawOval(84, 70, 10, 20);
-                     * g2d.fillOval(84, 70, 10, 20);
-                     * g2d.drawOval(105, 70, 10, 20);
-                     * g2d.fillOval(105, 70, 10, 20);
-                     * 
-                     * // Draw a square House to the bottom right of the sun
-                     * g2d.setColor(Color.GRAY);
-                     * g2d.drawRect(200, 200, 50, 50);
-                     * g2d.fillRect(200, 200, 50, 50);
-                     * g2d.setColor(Color.RED);
-                     * g2d.drawLine(200, 200, 225, 175);
-                     * g2d.drawLine(225, 175, 250, 200);
-                     * 
-                     * // Draw red door
-                     * g2d.drawRect(220, 235, 10, 15);
-                     * 
-                     * // Draw door outline
-                     * 
-                     * // draw logo
-                     */
                     g2d.setColor(Color.GRAY);
 
                     g2d.fillRect(50, 50, 100, 215);
