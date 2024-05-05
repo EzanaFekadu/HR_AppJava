e36090452fbb954de9048a645fa00a1141c5f3db
isabella-floyd <ifloyd@murraystate.edu>
2024-05-05 16:26:53 -0500

    comments

--- HRAppGUI.java
+++ HRAppGUI.java
@@ -4,31 +4,36 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class HRAppGUI extends JFrame {
-
+    // Instance Varaibles
     private JTextField txtEmployeeId, txtFirstName, txtLastName, txtAge;
     private JButton btnSave, btnRead, btnUpdate, btnDelete;
 
     public HRAppGUI() {
+        // Setting up the details
         setTitle("HR Web App");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(400, 200);
         setLayout(new GridLayout(5, 2, 10, 10));
 
+        // Creating headers
         JLabel lblEmployeeId = new JLabel("Employee ID:");
         JLabel lblFirstName = new JLabel("First Name:");
         JLabel lblLastName = new JLabel("Last Name:");
         JLabel lblAge = new JLabel("Age:");
 
+        // Creating the textField to get employee data
         txtEmployeeId = new JTextField();
         txtFirstName = new JTextField();
         txtLastName = new JTextField();
         txtAge = new JTextField();
 
+        // creating buttons
         btnSave = new JButton("Save");
         btnRead = new JButton("Read");
         btnUpdate = new JButton("Update");
         btnDelete = new JButton("Delete");
 
+        // Adding button action listeners
         btnSave.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -57,6 +62,7 @@ public class HRAppGUI extends JFrame {
             }
         });
 
+        // Adding everything to the frame
         add(lblEmployeeId);
         add(txtEmployeeId);
         add(lblFirstName);
@@ -70,6 +76,7 @@ public class HRAppGUI extends JFrame {
         add(btnUpdate);
         add(btnDelete);
 
+        // displaying the frame
         setVisible(true);
     }
 
